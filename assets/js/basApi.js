f2b374a91d1a535ccdcc96e9d976310b07e78dbc
pangyongkai <794089278@qq.com>
2021-01-08 09:13:53 +0800

    更改了统一接口

--- assets/js/basApi.js
+++ assets/js/basApi.js
@@ -4,7 +4,7 @@ $.ajaxPrefilter(function(options) {
     options.url = 'http://ajax.frontend.itheima.net' + options.url
 
     // 统一权限接口，设置headers请求头
-    if (options.url.indexOf !== -1) {
+    if (options.url.indexOf('/my') !== -1) {
         options.headers = {
             Authorization: localStorage.getItem('token') || ''
         }
